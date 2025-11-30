7a2e67188e8af47729f2dbdc898026be669a59c8
fadel benomar <fadel.benomar3241@gmail.com>
2025-11-30 13:26:06 +0100

    feat : Authentification via ios 26.1 done , creation et connection

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -14,15 +14,9 @@ class DefaultFirebaseOptions {
       case TargetPlatform.android:
         return android;
       case TargetPlatform.iOS:
-        throw UnsupportedError(
-          'DefaultFirebaseOptions have not been configured for ios - '
-              'you can reconfigure this by running the FlutterFire CLI again.',
-        );
+        return ios;
       case TargetPlatform.macOS:
-        throw UnsupportedError(
-          'DefaultFirebaseOptions have not been configured for macos - '
-              'you can reconfigure this by running the FlutterFire CLI again.',
-        );
+        return macos;
       case TargetPlatform.windows:
         throw UnsupportedError(
           'DefaultFirebaseOptions have not been configured for windows - '
@@ -47,4 +41,16 @@ class DefaultFirebaseOptions {
     projectId: 'smart-bidonville',
     storageBucket: 'smart-bidonville.firebasestorage.app',
   );
+
+  static const FirebaseOptions ios = FirebaseOptions(
+    apiKey: 'AIzaSyDV_AawMtatFi4sRU5ghZb2wtIXHc0GfYk',
+    appId: '1:709314346120:ios:c9a78d5fcfc7a87dd9b33d',
+    messagingSenderId: '709314346120',
+    projectId: 'smart-bidonville',
+    storageBucket: 'smart-bidonville.firebasestorage.app',
+    iosBundleId: 'com.example.smartBidonville',
+  );
+
+  // Pour macOS, si tu veux tester sur macOS, tu peux réutiliser la config iOS :
+  static const FirebaseOptions macos = ios;
 }
